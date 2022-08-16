50126c01e88ab8fa4e92a2fc4735bf3b6bc914e8
CNieblesR <camila.niebles@rootstrap.com>
2022-08-15 20:04:15 -0500

    Fixing error in build command

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
+import { grpcOptions } from './config/grpcOptions';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.GRPC,
+    options: grpcOptions,
+  });
+  app.listen();
 }
 bootstrap();
