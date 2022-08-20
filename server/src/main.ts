feea7889639592186db4c22cac1f3c894e686f56
CNieblesR <camila.niebles@rootstrap.com>
2022-08-20 17:31:22 -0500

    Database and grpc configuration

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,10 +4,15 @@ import { AppModule } from './app.module';
 import { grpcOptions } from './config/grpcOptions';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice(AppModule, {
-    transport: Transport.GRPC,
-    options: grpcOptions,
-  });
-  app.listen();
+  const app = await NestFactory.create(AppModule);
+  app.connectMicroservice(
+    {
+      transport: Transport.GRPC,
+      options: { ...grpcOptions },
+    },
+    { inheritAppConfig: true },
+  );
+  await app.startAllMicroservices();
+  await app.listen(process.env.ENV_PORT);
 }
 bootstrap();
