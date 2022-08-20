c8f66e246532d568a650e52e4368cc6a192464d9
CNieblesR <camila.niebles@rootstrap.com>
2022-08-20 17:27:27 -0500

    Database and grpc configuration

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfigService } from './config/typeOrm/typeOrm.service';
 import { AppController } from './controllers/app.controller';
 import { AppService } from './services/app.service';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigService })],
   controllers: [AppController],
   providers: [AppService],
 })
